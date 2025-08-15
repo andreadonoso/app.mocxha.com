dbefc8305aadb10df3c322a32397050e262eb862
Andrea Donoso <donosoandrea@icloud.com>
2025-08-15 00:16:23 -0400

    adding header component and alignment

--- mocxha/app/page.tsx
+++ mocxha/app/page.tsx
@@ -1,17 +1,10 @@
 import Image from "next/image";
+import Header from "./ui/header.tsx"
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <header className="flex">
-        <p>Home</p>
-        <p>Features</p>
-        <p>Pricing</p>
-        <p>About</p>
-        <p>Comparison</p>
-        <p>Start With SEO</p>
-        <p>Request Access</p>
-      </header>
+      <Header/>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-center">
         <p>Automate Your Business In One Click</p>
         <h1>Stop Juggling Software.</h1>
