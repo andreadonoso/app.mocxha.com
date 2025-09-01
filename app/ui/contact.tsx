e244a6f1c46ce6213cc8d23cfa335fb28cbce615
Andrea Donoso <donosoandrea@icloud.com>
2025-09-01 01:15:48 -0400

    added general contact content

--- app/ui/contact.tsx
+++ app/ui/contact.tsx
@@ -1,23 +1,36 @@
 "use client"
 import { Envelope, Phone, MapPin } from "@phosphor-icons/react";
 
+const items = [
+  {icon: <Envelope color="#000000" weight="fill" size={24}/>, description: "info@mocxha.com", action: "Email Us"},
+  {icon: <Phone color="#000000" weight="fill" size={24}/>, description: "+1 (786)-796-6757", action: "Call Us"},
+  {icon: <MapPin color="#000000" weight="fill" size={24}/>, description: "3300 NE 192 St, Aventura, FL 33180", action: "Get Direction"},
+]
+
 export default function Contact() {
     return(
-        <>
-        <Envelope/>
-        <Phone  color="#000000" weight="fill"/>
-        <MapPin/>
-        <p>Contact</p>
-        <h2>Ready to Discuss Your Specific Needs? Let's Connect.</h2>
-        <p>
-          Reach out for inquiries, support, or to schedule your personalized demo. Our team is ready to help you explore the power of Mocxha.
-        </p>
-        <p>info@mocxha.com</p>
-        <p>Email Us</p>
-        <p>+1 (786)-796-6757</p>
-        <p>Call Us</p>
-        <p>3300 NE 192 St, Aventura, FL 33180</p>
-        <p>Get Direction</p>
-        </>
+        <div className="py-20 flex gap-8">
+          <div className=" flex flex-col gap-4">
+            <p className="pill">Contact</p>
+            <h2>Ready to Discuss Your Specific Needs? Let's Connect.</h2>
+            <p>
+              Reach out for inquiries, support, or to schedule your personalized demo. Our team is ready to help you explore the power of Mocxha.
+            </p>
+            {items.map((item, i) => (
+              <div key={i} className="flex items-center gap-4 bg-[#E8E4E2] rounded-lg p-3 justify-between">
+                <div className="flex gap-4">
+                  {item.icon}
+                  <p>{item.description}</p>
+                </div>
+                <button className="btn btn-primary w-40">{item.action}</button>
+              </div>
+            ))}
+          </div>
+          <div className="bg-[#111111] text-white p-4 w-200">
+            <h5>30 Min Meeting - Moxcha</h5>
+            <p>A quick demo to see if we are the right fit for eachother</p>
+            <p>... embed api here...</p>
+          </div>
+        </div>
     )
 }
